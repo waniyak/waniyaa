1407e914d6d1289098de9465683f256d9c343526
ahad580 <ahad58040@gmail.com>
2023-07-15 18:37:26 +0500

    hello

---
+++ js/compare-data.js
@@ -0,0 +1,114 @@
+var product = [{
+    "id": 0,
+    "title": "",
+    "price": "",
+    "description": "",
+    "brand": "",
+    "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
+}, {
+    "id": 1,
+    "title": "SOFT TEDDY BEAR (Lego)",
+    "price": "575",
+    "description": "Comfortable Fur And Floppy Body Make This Stuffed Bear A Cuddly Friend And The Perfect Companion; Washable Teddy Bear Ideal For Hugging Or Using As A Pillow When Lounging, It Would Make A Great Gift For A Young Child Or For A Baby Shower. Valentine Teddy Bear",
+    "brand": "Lego",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/products/IMG_8780_b6e3870b-a965-40ef-b15e-8827ae212d2c_540x.jpg"
+},
+{
+    "id": 2,
+    "title": "SOFT TEDDY BEAR (Barbie)",
+    "price": "600",
+    "description": "Comfortable Fur And Floppy Body Make This Stuffed Bear A Cuddly Friend And The Perfect Companion; Washable Teddy Bear Ideal For Hugging Or Using As A Pillow When Lounging, It Would Make A Great Gift For A Young Child Or For A Baby Shower. Valentine Teddy Bear",
+    "brand": "Barbie",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/products/IMG_0009_c59262c8-06dc-4a26-a9b5-db2377d7a1c9_720x.jpg?v=1675851892"
+}, {
+    "id": 3,
+    "title": "MINI WATCH R/C CAR (Hotwheels)",
+    "price": "1,000",
+    "description": "This Watch Remote Control Toy Car has a silicone wrist strap and two buttons for different functions. The right button moves the car in a circle while the left button moves the vehicle ahead.Comes with a cover: The watch car comes with a dustproof cover, and the cover is opened with a button to protect the car from damage.Built-in light at the bottom, creates a unique lighting atmosphere.USB charging, convenient to use.The adjustable strap is comfortable to wear and fits toddlers to children's hands",
+    "brand": "Hotwheels",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/files/2_1643b7bc-9cf9-4bed-bdd4-8416f5132e16_1024x1024.jpg"
+},
+{
+    "id": 4,
+    "title": "MINI WATCH R/C POLICE CAR (Lego)",
+    "price": "700",
+    "description": "This Watch Remote Control Toy Police Car has a silicone wrist strap and four buttons for different functions. The right button moves the car in a circle while the left button moves the vehicle ahead.Comes with a cover: The watch car comes with a dustproof cover, and the cover is opened with a button to protect the car from damage.Built-in light at the bottom & top, creates a unique lighting atmosphere.USB charging, convenient to use.The adjustable strap is comfortable to wear and fits toddlers to children's hands",
+    "brand": "Lego",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/files/IMG_0682_2b92bc55-89f2-407d-9e24-2abc3526e147_540x.jpg?v=1687418337"
+}, 
+{
+    "id": 5,
+    "title": "FROG HAMMER GAME (Mattel)",
+    "price": "500",
+    "description": "Toyo Toys Offers You The Best Price For Super Frog Hammer Game For Kids With Express Shipping All Over The World. Improve childrens concentration and hand-eye coordination as they try and get the best score, kids  can keeping eye on Catching the frogs.",
+    "brand": "Mattel",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/files/z75427_2_540x.jpg"
+},{
+    "id": 6,
+    "title": "WHACK A FROG GAME (Lego)",
+    "price": "800",
+    "description": "Toyo Toys Offers You The Best Price For Super Frog Hammer Game For Kids With Express Shipping All Over The World.Is the perfect interactive toy for both children and adults to enjoy together, competing to see who can get a better score.Improve childrens concentration and hand-eye coordination as they try and get the best score they can keeping track of the flashing frogs.",
+    "brand": "Lego",
+    "image": "https://m.media-amazon.com/images/I/61WfbZRh7mS._AC_SX522_.jpg"
+}, 
+{
+    "id": 7,
+    "title": "LITTLE GIRLS DIY NAIL ART (Barbie)",
+    "price": "100",
+    "description": "This DIY nail art stamping printer is not edible Nail Polish & Fashion glitter upProducts have been tested by safety standardsCreate a cool look Design your nails Sweets. Let your childrens have healthy growth happy every day!Cosmetics Come and make yourself  beautiful",
+    "brand": "Barbie",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/products/z70846_2_540x.jpg"
+},{
+    "id": 8,
+    "title": "GIRLS DIY NAIL ART (Barbie)",
+    "price": "300",
+    "description": "Sparkles Your Nails offers a wide range of nail polish and fashion glitter products that have been rigorously tested to meet safety standards. Whether you're looking to create a cool and unique nail design or simply add some sparkle to your everyday look, our collection has you covered. We believe in promoting healthy growth and happiness for children, which is why we offer sweet-themed products that are safe for their use. In addition to nail products, we also provide a range of cosmetics to help you enhance your natural beauty.",
+    "brand": "Barbie",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/products/z26133_2_540x.jpg"
+}, 
+{
+    "id": 9,
+    "title": "DIE CAST BIKE MODEL HEAVY BIKE (Lego)",
+    "price": "450",
+    "description": "This model heavy bike boasts exquisite workmanship and features a convenient pull-back function. To activate it, simply press the car gently and pull it back slightly. Once released, the car will automatically move forward, providing an easy-to-use experience. The product stands out with its animalistic design, adding a touch of uniqueness. Additionally, the car incorporates real sound and light systems, enhancing its overall design and surpassing expectations. Its compact size and lightweight body make it easily portable.",
+    "brand": "(Lego)",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/files/z79023_4_540x.jpg"
+},{
+    "id": 10,
+    "title": "DIE CAST BIKE MODEL MOTO BIKE (Hotwheels)",
+    "price": "340",
+    "description": "Unleash the thrill of the road with our Die Cast Bike Model Moto Bike For Kids. This miniature marvel is designed to ignite the imaginations of young riders. Crafted with meticulous attention to detail, this toy captures the essence of a real moto bike on a smaller, more playful scale. It's time to hit the road and let their imagination ride wild!",
+    "brand": "(Hotwheels)",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/products/IMG_6079_f9cea011-41bc-44a2-8713-a1e013c06dc5_720x.jpg"
+}, 
+{
+    "id": 11,
+    "title": "WATER BLASTER TOY (Mattel)",
+    "price": "260",
+    "description": "Introducing the ultimate water gun for kids! With its pump action and hyper blasting power.Designed with a large capacity tank,allowing kids to stay in the action longer,drenching their targets with unmatched precision. Not only is this water gun loads of fun, but it also boasts a cool and eye-catching design that will make kids the envy of their friends.",
+    "brand": "Mattel",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/products/z75457_2_540x.jpg"
+},{
+    "id": 12,
+    "title": "WATER BLASTER TOY (Barbie)",
+    "price": "160",
+    "description": "Kids enjoy & fun with this water gun A large capacity tank so you can refill less Pump action hyper water gun!When you've got your target in your sights, blast awayGet kids this amazing water gun and let them have the best summertime of their life This is a fun and cool-looking water gun",
+    "brand": "Barbie",
+    "image": "https://www.itinternational.pk/wp-content/uploads/2020/12/Microsoft-Xbox-360-Ultra-Slim-250-GB-Price-in-Pakistan.jpg"
+}, 
+{
+    "id": 13,
+    "title": "DOLL WITH WALKER PLAY SET (Barbie)",
+    "price": "350",
+    "description": "The Xbox One S, you can play a vast library of Xbox One games, including both physical discs and digital downloads. It supports high-definition gaming with improved graphics and faster performance. The console also supports HDR (High Dynamic Range) technology, which enhances the visual quality of compatible games, making them more vibrant and lifelike.",
+    "brand": "Barbie",
+    "image": "https://5.imimg.com/data5/FP/MC/PK/SELLER-14158318/microsoft-xbox-one-s-all-digital-edition-gaming-console-500x500.jpg"
+},{
+    "id": 14,
+    "title": "MULTI FUNCTION BABY WALKER (Barbie)",
+    "price": "900",
+    "description": "The baby walker features rocking and jumping functions, a rotating seat for 360-degree movement, 2 fun trays with music, a removable tray for feeding, suitability from 6 to 12 months, 3 levels of height adjustment, and a wipe-clean, washable seat cushion.",
+    "brand": "Barbie",
+    "image": "https://cdn.shopify.com/s/files/1/0102/7755/2164/products/fa63844_720x.jpg"
+}, 
+]
